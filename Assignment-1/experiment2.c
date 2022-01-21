91c566e2be4cc2fff70923658bc112a8985be4ec
Arvind136 <90745235+Arvind136@users.noreply.github.com>
2022-01-21 14:54:54 +0530

    Write a program to check whether a given input is vowel or not.

---
+++ Assignment-1/experiment2.c
@@ -0,0 +1,30 @@
+// Write a program to print the greatest number among 3 given numbers.
+
+#include <stdio.h>
+int main()
+{
+int num1, num2, num3;
+printf(" Enter the number1 = ");
+scanf("%d", &num1);
+printf("\n Enter the number2 = ");
+scanf("%d", &num2);
+printf("\n Enter the number3 = ");
+scanf("%d", &num3);
+  
+if (num1 > num2)
+{
+       if (num1 > num3){
+               printf("\n %d is the greatest number \n",num1);
+       }
+      else{
+               printf("\n %d is the greatest number \n",num3);
+       }
+}
+else if (num2 > num3){
+       printf("\n %d is the greatest number \n",num2);
+}
+else {
+       printf("\n %d is the greatest number \n",num3);
+}
+return 0;
+}
