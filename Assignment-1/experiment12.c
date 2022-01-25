7ecf4a4ee67b1f3761ff327e84a64ec727f6e864
Arvind136 <90745235+Arvind136@users.noreply.github.com>
2022-01-25 11:42:04 +0530

    WAP to find whether a given number is palindrome or not.

---
+++ Assignment-1/experiment12.c
@@ -0,0 +1,21 @@
+// Write a program to find whether a given number is palindrome or not.
+
+#include<stdio.h>  
+int main(){    
+int n,n1,rev=0,rem;    
+printf("Enter the number=");    
+scanf("%d",&n);    
+n1=n;    
+while(n>0){    
+rem=n%10;    
+rev=(rev*10)+rem;    
+n=n/10;    
+}    
+if(rev==n1){  
+printf("%d is a Palindrome number ",n1);
+           }    
+else{  
+printf("%d is not a Palindrome number",n1);  
+} 
+return 0;  
+}   
